a6094bef04d6338bf053e6fde1efbaae1fa53e04
Avan80 <mdab7253@gmail.com>
2026-05-11 23:44:38 +0300

    Add succesful login detection

--- parser.py
+++ parser.py
@@ -5,18 +5,31 @@ ip_counter = {}
 with open('test.log', 'r') as file:
     for line in file:
         if 'Failed password' in line:
-            print(line.strip().split())
             parts = line.strip().split()
 
             if "from" in parts:
                 index = parts.index("from") + 1
                 ip = parts[index]
-                if  ip not in ip_counter:
-                    ip_counter[ip] = 1
-                else:
-                    ip_counter[ip] += 1
+                ip_counter[ip] = ip_counter.get(ip, 0) + 1
 threshold = 5
-print("\nPotential brute-force sources \n")
+print("\nPotential brute-force sources:\n")
 for ip, count in ip_counter.items():
     if count >= threshold:
         print(f"{ip} failed to log in {count} times")
+
+successful_logins = {}
+
+with open('test.log', 'r') as file:
+    for line in file:
+        if 'Accepted password' in line:
+            parts = line.strip().split()
+
+            if "for" in parts:
+                index = parts.index("for") + 1
+                username = parts[index]
+                successful_logins[username] = successful_logins.get(username, 0) + 1
+
+print("\nSuccessful logins:\n")
+for user, count in successful_logins.items():
+    print(f"{user}: {count} successful logins")
+

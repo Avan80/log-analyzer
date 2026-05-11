8c28966e654b29504c803e70d20d70716b4c0ca2
Avan80 <mdab7253@gmail.com>
2026-05-11 23:11:27 +0300

    Add bruteforce detection

--- parser.py
+++ parser.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
-with open('/var/log/auth.log', 'r') as file:
+ip_counter = {}
+
+with open('test.log', 'r') as file:
     for line in file:
         if 'Failed password' in line:
-            print(line)
+            print(line.strip().split())
+            parts = line.strip().split()
+
+            if "from" in parts:
+                index = parts.index("from") + 1
+                ip = parts[index]
+                if  ip not in ip_counter:
+                    ip_counter[ip] = 1
+                else:
+                    ip_counter[ip] += 1
+threshold = 5
+print("\nPotential brute-force sources \n")
+for ip, count in ip_counter.items():
+    if count >= threshold:
+        print(f"{ip} failed to log in {count} times")
